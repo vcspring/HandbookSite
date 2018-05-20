0fa0d7cdcb4e8e3f8c49c8f7d560a9a94a4f8b9b
vcspring <2802594081@qq.com>
2018-05-20 10:29:37 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Unreal Handbook') - Unreal 笔记</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
